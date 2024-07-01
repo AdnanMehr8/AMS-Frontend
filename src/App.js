2d010c0b3c53f8ae14420366f92185a730e22f3c
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2024-07-01 05:58:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,105 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// import { useSelector } from "react-redux";
-// import useAutoLogin from "./hooks/useAutoLogin";
-// import StudentDashboard from './components/Dashboard/StudentDashboard';
-// import AdminDashboard from './components/Dashboard/AdminDashboard';
-// import Protected from './components/protected/Protected';
-// import Navbar from './components/Layout/Header';
-// import Footer from './components/Layout/Footer';
-// import Register from './components/Auth/Register';
-// import Login from './components/Auth/Login';
-// import EditProfile from './components/Profile/EditProfile';
-// import RequestLeave from './components/Leave/RequestLeave';
-// import ViewLeaves from './components/Leave/ViewLeaves';
-// import MarkAttendance from './components/Attendance/MarkAttendance';
-// import ViewAttendance from './components/Attendance/ViewAttendance';
-// import ApproveLeaveRequests from './components/Admin/ApproveLeaveRequests';
-// import ManageAttendance from './components/Admin/ManageAttendance';
-// import SetGradingCriteria from './components/Admin/SetGradingCriteria';
-// import useTokenRefresh from './hooks/useAutoRefresh';
-
-// function App() {
-//   const isAuth = useSelector(state => state.user.auth);
-//   const loading = useAutoLogin();
-//   useTokenRefresh();
-
-//   return loading ? 'loading...' : (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/login" element={<Login />} />
-        
-//         <Route path="/student-dashboard" element={
-//           <Protected isAuth={isAuth}>
-//               <StudentDashboard />
-//           </Protected>
-//         } />
-        
-//         <Route path="/update-profile" element={
-//           <Protected isAuth={isAuth}>
-//             <EditProfile />
-//           </Protected>
-//         } />
-        
-//         <Route path="/admin-dashboard" element={
-//           <Protected isAuth={isAuth}>
-//               <AdminDashboard />
-//           </Protected>
-//         } />
-        
-//         <Route path="/submit-leave" element={
-//           <Protected isAuth={isAuth}>
-//             <RequestLeave />
-//           </Protected>
-//         } />
-        
-//         <Route path="/leave-requests" element={
-//           <Protected isAuth={isAuth}>
-//             <ViewLeaves />
-//           </Protected>
-//         } />
-        
-//         <Route path="/mark" element={
-//           <Protected isAuth={isAuth}>
-//             <MarkAttendance />
-//           </Protected>
-//         } />
-        
-//         <Route path="/all-attendance" element={
-//           <Protected isAuth={isAuth}>
-//             <ViewAttendance />
-//           </Protected>
-//         } />
-        
-//         <Route path="/leave" element={
-//           <Protected isAuth={isAuth}>
-//             <ApproveLeaveRequests />
-//           </Protected>
-//         } />
-        
-//         <Route path="/students-attendance/manage" element={
-//           <Protected isAuth={isAuth}>
-//             <ManageAttendance />
-//           </Protected>
-//         } />
-        
-//         <Route path="/grading" element={
-//           <Protected isAuth={isAuth}>
-//             <SetGradingCriteria />
-//           </Protected>
-//         } />
-        
-//         <Route path="*" element={<Navigate to="/" />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
@@ -123,7 +21,6 @@ import useTokenRefresh from './hooks/useAutoRefresh';
 
 function App() {
   const isAuth = useSelector(state => state.user.auth);
-  const role = useSelector(state => state.user.role);
   const loading = useAutoLogin();
   useTokenRefresh();
 
@@ -131,79 +28,70 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        {/* Default route for unauthenticated users */}
-        {!isAuth && <Route path="/" element={<Navigate to="/login" />} />}
-
-        {/* Role-based redirects */}
-        {isAuth && role === 'student' && <Route path="/" element={<Navigate to="/student-dashboard" />} />}
-        {isAuth && role === 'admin' && <Route path="/" element={<Navigate to="/admin-dashboard" />} />}
-
-        {/* Authentication routes */}
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-
-        {/* Protected routes */}
+        
         <Route path="/student-dashboard" element={
           <Protected isAuth={isAuth}>
-            <StudentDashboard />
+              <StudentDashboard />
           </Protected>
         } />
-
-        <Route path="/admin-dashboard" element={
-          <Protected isAuth={isAuth}>
-            <AdminDashboard />
-          </Protected>
-        } />
-
+        
         <Route path="/update-profile" element={
           <Protected isAuth={isAuth}>
             <EditProfile />
           </Protected>
         } />
-
+        
+        <Route path="/admin-dashboard" element={
+          <Protected isAuth={isAuth}>
+              <AdminDashboard />
+          </Protected>
+        } />
+        
         <Route path="/submit-leave" element={
           <Protected isAuth={isAuth}>
             <RequestLeave />
           </Protected>
         } />
-
+        
         <Route path="/leave-requests" element={
           <Protected isAuth={isAuth}>
             <ViewLeaves />
           </Protected>
         } />
-
+        
         <Route path="/mark" element={
           <Protected isAuth={isAuth}>
             <MarkAttendance />
           </Protected>
         } />
-
+        
         <Route path="/all-attendance" element={
           <Protected isAuth={isAuth}>
             <ViewAttendance />
           </Protected>
         } />
-
+        
         <Route path="/leave" element={
           <Protected isAuth={isAuth}>
             <ApproveLeaveRequests />
           </Protected>
         } />
-
+        
         <Route path="/students-attendance/manage" element={
           <Protected isAuth={isAuth}>
             <ManageAttendance />
           </Protected>
         } />
-
+        
         <Route path="/grading" element={
           <Protected isAuth={isAuth}>
             <SetGradingCriteria />
           </Protected>
         } />
-
-        {/* Fallback route */}
+        
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
@@ -212,3 +100,4 @@ function App() {
 }
 
 export default App;
+
